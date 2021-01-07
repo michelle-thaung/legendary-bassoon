dcefa663e6cf6d0b9407d8186ff3fbabb3880eae
Pak Ming Lau <pak.plau@gmail.com>
2021-01-06 22:17:52 -0500

    Complete login and registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,59 @@
 from flask import Flask, render_template, request, session
+from db import setup, entry_exists, register_user, display, check_credentials
+import os, sqlite3
+
+app = Flask(__name__)
+
+setup()
 
 @app.route("/", methods=["GET", "POST"])
 def root():
-    if(credentialsWork()):
+    if(False):
         return render_template("general.html")
     return render_template("login_or_register.html")
 
-@app.rout("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    
+    if(request.method == "POST"):
+        register_username = request.form["register_username"]
+        register_password = request.form["register_password"]
+        confirm = request.form["confirm"]
+        blog_name = request.form["blog_name"]
+        description = request.form["description"]
+        if(entry_exists(register_username, "users", 0)):
+            return render_template("register.html", error="Username already exists!")
+        elif(confirm != register_password):
+            return render_template("register.html", error="Passwords should match!")
+        elif(register_username == "" or register_password == "" or confirm == "" or blog_name == "" or description == ""):
+            return render_template("register.html", error="Please fill in all of the boxes below:")
+        elif(not register_username.isalnum()):
+            return render_template("register.html", error="The username should only contain alphanumeric characters!")
+        register_user(register_username, register_password, blog_name, description)
+        return render_template("registration_successful.html")
+    return render_template("register.html", error="")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+        username = session["username"] = request.form["username"]
+        password = session["password"] = request.form["password"]
+        if(username == "" or password == ""):
+            return render_template("login.html", error="Please fill in your credentials")
+        elif(not(check_credentials(session["username"], session["password"]))):
+            return render_template("login.html", error="Wrong credentials!")
+    if("username" in session and "password" in session and check_credentials(session["username"], session["password"])):
+        return render_template("general.html")
+    return render_template("login.html", error="")
+
+@app.route("/user", methods=["GET"])
+def user():
+    return render_template("user.html", collection=display("users"))
+
+@app.route("/blog", methods=["GET"])
+def blog():
+    return render_template("blog.html", collection=display("blogs"))
+
+if(__name__ == "__main__"):
+    app.secret_key = os.urandom(32)
+    app.debug = True
+    app.run()
---
+++ app/db.py
@@ -0,0 +1,52 @@
+import sqlite3
+from datetime import datetime
+
+db_file = "databases/fruit_for_blogs.db"
+text_factory = str
+now = datetime.now()
+
+def setup():
+    db = sqlite3.connect(db_file)
+    db.text_factory = text_factory
+    c = db.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT NOT NULL)")
+    c.execute("CREATE TABLE IF NOT EXISTS blogs (user TEXT, blog TEXT NOT NULL)")
+    db.commit()
+    db.close()
+
+def entry_exists(entry, table, n):
+    db = sqlite3.connect(db_file)
+    db.text_factory = text_factory
+    c = db.cursor()
+    exist = False
+    for row in c.execute("SELECT * FROM " + table):
+        exist = exist or (entry == row[n])
+    db.close()
+    return exist
+
+def check_credentials(username, password):
+    db = sqlite3.connect(db_file)
+    db.text_factory = text_factory
+    c = db.cursor()
+    exist = False
+    for row in c.execute("SELECT * FROM users"):
+        exist = exist or (username == row[0] and password == row[1])
+    db.close()
+    return exist
+
+def register_user(username, password, title, description):
+    db = sqlite3.connect(db_file)
+    db.text_factory = text_factory
+    c = db.cursor()
+    c.execute("INSERT INTO users VALUES ('" + username + "' ,'" + password + "')")
+    c.execute("INSERT INTO blogs VALUES ('" + username + "' ,'" + title + "')")
+    c.execute("CREATE TABLE " + username + "_" + title +  " (title TEXT, body TEXT, time TEXT)")
+    c.execute("INSERT INTO " + username + "_" + title + " VALUES ('" + title + "' ,'" + description + "' ,'" + now.strftime("%d/%m/%Y %H:%M:%S") + "')")
+    db.commit()
+    db.close()
+
+def display(table):
+    db = sqlite3.connect(db_file)
+    c = db.cursor()
+    return tuple(c.execute("SELECT * FROM " + table))
+    db.close()
